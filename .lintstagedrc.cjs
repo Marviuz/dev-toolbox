da8a803e7ca7bcafe92ad703f0295bb57d8cf94a
Marviuz <marviuz.gaming@gmail.com>
2024-09-19 21:40:03 +0800

    initial commit

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const lint = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const format = 'prettier --write';
+
+module.exports = {
+  '*.{cjs,mjs,js,jsx,ts,tsx}': [format, lint],
+  '*.json': [format],
+};
