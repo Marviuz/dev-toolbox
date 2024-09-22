2cebd704deefc5c8996de5537a97b39b7456aaa1
Marviuz <marviuz.gaming@gmail.com>
2024-09-22 16:25:38 +0800

    fix: env check

--- src/env.mjs
+++ src/env.mjs
@@ -9,4 +9,5 @@ export const env = createEnv({
     NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL:
       process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
   },
+  skipValidation: process.env.SKIP_VALIDATION === 'true',
 });
